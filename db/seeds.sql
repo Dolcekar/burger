58b96c6bbc2c134f8f23b9ca3beec8a8bc115de2
Carla Cox <astronomykar@gmail.com>
2018-08-11 15:41:21 -0400

    code

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Nuts Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Greens Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie-veggie Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portabella Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sweet Potato Burger', true);
